faa35b0d844a38e2f1f2bd266e6f436cc5d50f9e
stan <smondesi@e-r9-p1.s19.be>
2019-10-01 14:47:01 +0200

    norme palette

--- srcs/palette.c
+++ srcs/palette.c
@@ -1,23 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   palette.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smondesi <smondesi@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/01 14:46:34 by smondesi          #+#    #+#             */
+/*   Updated: 2019/10/01 14:46:46 by smondesi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../incl/fdf.h"
 
-void set_color(t_fdf *fdf, int *color, int z)
+void	set_color(t_fdf *fdf, int *color, int z)
 {
 	double percentage;
+
 	if (z >= 0)
-		percentage = (double)z/(double)fdf->map->max_height;
+		percentage = (double)z / (double)fdf->map->max_height;
 	else
-		percentage = (double)z/(double)fdf->map->min_height;
+		percentage = (double)z / (double)fdf->map->min_height;
 	if (fdf->palette == 0)
 	{
 		if (z == 0)
 			*color = get_int_color(0, 255, 0);
 		else if (z < 0)
-			*color = get_int_color(0 ,255*(1.0-percentage),255);
+			*color = get_int_color(0, 255 * (1.0 - percentage), 255);
 		else
-			*color = get_int_color(255* (percentage),255*(1.0-percentage),0);
+			*color = get_int_color(255 * (percentage), 255 *
+				(1.0 - percentage), 0);
 	}
 	else if (fdf->palette == 1)
 	{
-		*color = get_int_color(255*(1.0-percentage),255*(percentage),255);
+		*color = get_int_color(255 *
+				(1.0 - percentage), 255 * (percentage), 255);
 	}
 }
